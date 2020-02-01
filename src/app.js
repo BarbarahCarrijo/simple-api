195c22bb463d83108c5f3b7adc24649600c98124
Barbara Carrijo <barbarahcarrijo@gmail.com>
2020-02-01 20:58:43 -0300

    include middlewares to app

--- src/app.js
+++ src/app.js
@@ -2,5 +2,6 @@ const app = require('express')();
 const consign = require('consign');
 
 consign({ verbose: true, locale: 'pt-br', cwd: 'src' })
-    .include('server')
+    .include('middlewares')
+    .then('server')
     .into(app);
